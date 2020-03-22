b3b825a8d9776f1567387962475ebda6a67ff0d7
orangepuer <rkhpuer@gmail.com>
2020-03-22 15:15:20 +0300

    Make a reducer refactoring

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -41,7 +41,7 @@ class BookListContainer extends Component {
   }
 }
 
-const mapStateToProps = ({books, loading, error}) => {
+const mapStateToProps = ({bookList: {books, loading, error}}) => {
   return {books, loading, error}
 };
 
